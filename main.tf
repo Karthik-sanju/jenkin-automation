3823d00e90b0d22a2eaacdcb9c0d70bc4609e39a
Karthik kumar reddy <88179147+Karthik-sanju@users.noreply.github.com>
2024-03-23 14:24:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-resource "aws_launch_configuration" "web-server" {
-    image_id           =  "ami-01387af90a62e3c01"
+resource "aws_launch_configuration" "web_server_as" {
+    image_id           = "ami-01387af90a62e3c01"
     instance_type = "t2.micro"
     key_name = "docker"
 }
    
 
 
-  resource "aws_elb" "aws_elb"{
+  resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
-     security_groups = [aws_security_group.web_server1.id]
-     subnets = ["subnet-0faae7713f4e2f438", "subnet-0fcfbc8b256444488"]
+     security_groups = [aws_security_group.web_server.id]
+     subnets =  ["subnet-0faae7713f4e2f438", "subnet-0fcfbc8b256444488"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
@@ -20,14 +20,14 @@ resource "aws_launch_configuration" "web-server" {
       Name = "terraform-elb"
     }
   }
-resource "aws_autoscaling_group" "web-server-asg" {
+resource "aws_autoscaling_group" "web_server_asg" {
     name                 = "web-server-asg"
-    launch_configuration = aws_launch_configuration.web-server.name
+    launch_configuration = aws_launch_configuration.web_server_as.name
     min_size             = 1
     max_size             = 3
     desired_capacity     = 2
     health_check_type    = "EC2"
-    load_balancers       = [aws_elb.aws_elb.name]
+    load_balancers       = [aws_elb.web_server_lb.name]
     availability_zones    = ["us-esat-2a", "us-east-2b"] 
     
   }
